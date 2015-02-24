b472c0ee785abdb0459bf2b1a31eb5dde02f7d17
mihuie <huie.michael@gmail.com>
2015-02-24 03:38:19 +0000

    trouble

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import logging
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://action@localhost/action'
+
 db = SQLAlchemy(app)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 from app import views, models
+from app.models import Profiles
+from app import db
+
+
+db.create_all()
--- app/views.py
+++ app/views.py
@@ -83,13 +83,13 @@ def profile():
 def show_user(userid):
     user = Profiles.query.filter_by(userid=userid).first_or_404()
     
-#     if request.headers['Content-Type'] == 'application/json': 
-#       return jsonify(profile_add_on = user.profile_add_on,
-#                      age = user.age,
-#                      sex = user.gender,
-#                      image = user.image,
-#                      username = user.username,
-#                      user_id = userid)
+    if request.headers['Content-Type'] == 'application/json': 
+      return jsonify(profile_add_on = user.profile_add_on,
+                     age = user.age,
+                     sex = user.gender,
+                     image = user.image,
+                     username = user.username,
+                     user_id = userid)
 
     return render_template('userprofile.html', user=user, filename = user.image)
 
@@ -98,16 +98,16 @@ def show_user(userid):
 @app.route('/profiles/', methods=['GET'])
 def show_users():
   users = Profiles.query.all()
-#   user_list = {}
-#   user_list ['users'] = []
-#   if request.method == 'GET' and request.headers['Content-Type'] == 'application/json':
-#     for u in users:
-#       tmp = {
-#         'username': u.username,
-#         'user_id': u.userid
-#       }
-#       user_list['users'].append(tmp)
-#     return jsonify(user_list)
+  user_list = {}
+  user_list ['users'] = []
+  if request.method == 'GET' and request.headers['Content-Type'] == 'application/json':
+    for u in users:
+      tmp = {
+        'username': u.username,
+        'user_id': u.userid
+      }
+      user_list['users'].append(tmp)
+    return jsonify(user_list)
   return render_template('profiles.html', users=users)
 
 ###
