cb4757935777bac89db583aba8dbe5fbbdc4e130
mihuie <huie.michael@gmail.com>
2015-02-24 14:06:30 +0000

    reverting to working app

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from app import views, models
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://action@localhost/action'
-
 db = SQLAlchemy(app)
-# db.create_all()
 
-
+from app import views, models
 
 
 
