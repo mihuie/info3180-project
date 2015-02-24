fc9c61c7e495eec9884dc10ea09cdedcd7043eff
mihuie <huie.michael@gmail.com>
2015-02-24 18:36:44 +0000

    working on db

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+# import os
+# from os import environ
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get['DATABASE_URL']
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://action@localhost/action'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://diosqsnmxxhrit:kRQOFycM4Xw_65etJEN3mh_fV9@ec2-50-17-202-29.compute-1.amazonaws.com:5432/de8p87fvfvr5o4'
+
 db = SQLAlchemy(app)
+db.create_all()
 
 from app import views, models
 
