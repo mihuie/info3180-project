171175bcc003c332d84f6db3f300cbe8265581a2
mihuie <huie.michael@gmail.com>
2015-02-24 03:48:19 +0000

    trouble

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,15 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import logging
-import sys
+# import sys
+from app import views, models
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://action@localhost/action'
 
 db = SQLAlchemy(app)
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
-
-from app import views, models
-from app.models import Profiles
-from app import db
+# app.logger.addHandler(logging.StreamHandler(sys.stdout))
+# app.logger.setLevel(logging.ERROR)
 
 
-db.create_all()
+
