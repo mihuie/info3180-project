309262fb195ca5b41e3a0d65b09713ab38f5eefe
mihuie <huie.michael@gmail.com>
2015-02-24 17:00:36 +0000

    troubleshooting 1

--- app/views.py
+++ app/views.py
@@ -58,10 +58,11 @@ def profile():
     form = CreateUserForm()
     if request.method == "POST" and form.validate():
       
-#        if (Profiles.query.filter_by(username = form.username.data).first() is None):
+       if (Profiles.query.filter_by(username = form.username.data).first() is None):
            
-#           userid = createID()#generate user id
-#           profile_add_on = timeinfo()#gets today's date
+          userid = createID()#generate user id
+          profile_add_on = timeinfo()#gets today's date
+          return '{}, {}'.format(userid, profile_add_on)
 
 #           filename = secure_filename(userid)
 #           form.image.data.save(UPLOAD_FOLDER + filename)
