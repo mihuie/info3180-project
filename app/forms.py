69ed0f5198790fb59ebd0edd90a0519b1c888e7e
mihuie <huie.michael@gmail.com>
2015-02-24 02:26:08 +0000

    troubleshooting

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,7 @@ from flask_wtf.file import FileField
 class CreateUserForm(Form):
     username = StringField('Username', validators=[Length(min=4, max=25)])
     firstname = StringField('First Name', validators=[Length(min=4, max=25)])
-    lastname = StringField('Last Name', validators=[Length(min=4, max=25)])
+    lastname = StringField('Last Name', validators=[Length(min=2, max=25)])
     age = IntegerField('Age', validators=[NumberRange(min=16, max=99)])
     gender = SelectField('Gender', choices=[('male', 'Male'), ('Female','Female')])    
     image = FileField('Image File', validators=[Required()])
