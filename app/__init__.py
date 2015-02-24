2ea3bf51d7ebcd24dfa426c1f2d07a0510d53ace
mihuie <huie.michael@gmail.com>
2015-02-24 17:52:03 +0000

    import db to heroku1

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://action@localhost/action'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://action@localhost/action'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 from app import views, models
