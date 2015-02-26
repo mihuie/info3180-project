810d8ad7d77b9c836aee02f9ee82bbb4f8ae8f6b
mihuie <huie.michael@gmail.com>
2015-02-26 15:50:49 +0000

    test api

--- app/views.py
+++ app/views.py
@@ -87,15 +87,17 @@ def show_users():
   users = Profiles.query.all()
   user_list = {}
   user_list ['users'] = []
-  return render_template('profiles.html', users=users)
-  if request.method == 'GET'and request.headers['Content-Type'] == 'application/json':
-    for u in users:
-      tmp = {
-        'username': u.username,
-        'user_id': u.userid
-      }
-      user_list['users'].append(tmp)
-    return jsonify(user_list)
+  
+  if request.method == 'GET':
+    if request.headers['Content-Type'] == 'application/json':
+      for u in users:
+        tmp = {
+          'username': u.username,
+          'user_id': u.userid
+        }
+        user_list['users'].append(tmp)
+      return jsonify(user_list)
+    return render_template('profiles.html', users=users)
   
 
 ###
